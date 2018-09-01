3bf253ab4d1268197f371804ff43b3f290f2e4cb
Jeferson Farias <jeferson.unisolucao.com.br>
2018-09-01 17:11:23 -0300

    Admin Page configuração

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,17 +7,20 @@ use Rain\Tpl;
 class Page {
 
 	private $tpl;
+	private $options = [];
 	private $defaults = [
 		"data"=>[]
 
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/")
+
+	{
 
 		$this->options = array_merge($this->defaults, $opts );
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false 
 	);
