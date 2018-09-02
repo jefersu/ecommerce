ecb5465c5fe5f4f132b93b986ec5768f52c36e25
Jeferson Farias <jeferson.unisolucao.com.br>
2018-09-01 23:21:59 -0300

    Revert "Admin Page configuração"
    This reverts commit 3bf253ab4d1268197f371804ff43b3f290f2e4cb.

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,20 +7,17 @@ use Rain\Tpl;
 class Page {
 
 	private $tpl;
-	private $options = [];
 	private $defaults = [
 		"data"=>[]
 
 	];
 
-	public function __construct($opts = array(), $tpl_dir = "/views/")
-
-	{
+	public function __construct($opts = array()){
 
 		$this->options = array_merge($this->defaults, $opts );
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false 
 	);
